b1030a4bfb68145505af30b0141f5e8e9f37ee56
Will King <kingwil@users.noreply.github.com>
2020-09-03 10:46:49 +0100

    IP Named Locations to CSV in Blob Storage

---
+++ Logic App Named Locations/Assign-MSI.ps1
@@ -0,0 +1,13 @@
+﻿Connect-AzureAD
+
+#Object ID of the Logic App MSI
+$managedIdentityId = "<guid>" 
+
+# MS Graph API object ID
+$msGraph = Get-AzureADServicePrincipal -Filter "AppID eq '00000003-0000-0000-c000-000000000000'"
+
+#Policy.Read.All permission
+$roleId = $msGraph.AppRoles| Where-Object {$_.value -eq "Policy.Read.All"}
+
+
+New-AzureADServiceAppRoleAssignment -ObjectId $managedIdentityId -PrincipalId $managedIdentityId -ResourceId $msGraph.ObjectId -Id $roleId.Id
